3be7cde22b5e39e0013f700542646f08c421d2e2
shisaq <shisaq@gmail.com>
2017-02-16 13:45:05 -0500

    feat: now a user can register by emitting his name, then he can see his name in the users list. Known issue: he cannot see others for now. Other socket events need to be updated by using node.js features, too.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
-module.exports = function(io) {
+module.exports = (io) => {
     var express = require('express');
     var router = express.Router();
 
-    io.on('connection', function(socket) {
-        console.log('hi');
+    // socket.io API
+    io.on('connection', (socket) => {
+        console.log('Server connected successfully - ' +
+            new Date().toLocaleString());
+
+        socket.on('disconnect', () => {
+            console.log('Client disconnected - ' +
+                new Date().toLocaleString());
+        });
+
+        // a user presses enter after input a valid username
+        socket.on('is_online', (name) => {
+            socket.emit('online_name', name, broadcast=true);
+        });
+
+        // `inviter` selects `guest`, sending 2 usernames to server
+        socket.on('build_private_room', (names) => {
+            console.log('The inviter is: [' + names['inviter'] +
+                '], the guest is: [' + names['guest'] + ']');
+            var r = [names['inviter'], names['guest']];
+            r.sort();
+            var room = r.join('');
+            console.log(room);
+
+            socket.emit('invite_match_user', {
+                'inviter': names['inviter'],
+                'guest': names['guest'],
+                'room': room,
+                'isActive': true
+            }, broadcast=true);
+        });
+
+        // 2 filtered users emit this event, then join room
+        socket.on('join_private_room', (data) => {
+            join_room(data['room']);
+            console.log('Users: ' + data['inviter'] + ', ' +
+                data['guest'] + ' joined the room [' +
+                data['room'] + '].');
+        });
+
+        // send private messages
+        socket.on('private_message', (data) => {
+            socket.emit('room_message', data, room=data['room']);
+        });
     });
 
     return router;
